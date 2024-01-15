6de3171c0d4681a984a9f5bd588e34a40fd9222f
Nick Tammadge <nicktammadge@gmail.com>
2024-01-14 21:00:14 -0800

    Re-organize database functions for better themeing/endpoint relevance

--- database/chirp.go
+++ database/chirp.go
@@ -1,6 +1,66 @@
+// Defines the Chirp type and database functions for interacting with Chrips
+
 package database
 
 type Chirp struct {
 	Id   int    `json:"id"`
 	Body string `json:"body"`
+}
+
+// Creates a new chirp and saves it to the database
+func (db *DB) CreateChirp(body string) (Chirp, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return Chirp{}, err
+	}
+	if dbStructure.Chirps == nil {
+		dbStructure.Chirps = map[int]Chirp{}
+	}
+
+	id := len(dbStructure.Chirps) + 1
+	chirp := Chirp{Id: id, Body: body}
+
+	dbStructure.Chirps[chirp.Id] = chirp
+	err = db.writeDB(dbStructure)
+	if err != nil {
+		return Chirp{}, err
+	}
+
+	return chirp, nil
+}
+
+// Gets a chirp by its id, if it exists
+func (db *DB) GetChirp(id int) (chirp Chirp, found bool, err error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return Chirp{}, false, err
+	}
+
+	if dbStructure.Chirps == nil || len(dbStructure.Chirps) == 0 {
+		return Chirp{}, false, nil
+	}
+	chirp, found = dbStructure.Chirps[id]
+	if !found {
+		return Chirp{}, false, nil
+	}
+	return chirp, true, nil
+}
+
+// Gets all of the existing Chirps
+func (db *DB) GetChirps() ([]Chirp, error) {
+	dbStructure, err := db.loadDB()
+	if err != nil {
+		return nil, err
+	}
+
+	if dbStructure.Chirps == nil || len(dbStructure.Chirps) == 0 {
+		return []Chirp{}, nil
+	}
+
+	chirps := []Chirp{}
+	for _, chirp := range dbStructure.Chirps {
+		chirps = append(chirps, chirp)
+	}
+
+	return chirps, nil
 }
